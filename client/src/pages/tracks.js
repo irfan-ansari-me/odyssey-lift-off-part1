ab72a710eef8f447ca140bb9bd9b07c37e99a347
Irfan Ansari <irfan.ansari.me@gmail.com>
2023-07-02 19:23:55 +0530

    Graphql code for client

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from "@apollo/client";
+import TrackCard from '../containers/track-card'
+import QueryResult from '../components/query-result'
+
+const TRACKS = gql`
+  query GetTrack {
+    tracksForHome {
+      id
+      length
+      author {
+        id
+        name
+        photo
+      }
+      modulesCount
+      thumbnail
+      title
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult loading={loading} error={error} data={data}>
+        {data?.tracksForHome?.map(track => {
+          return <TrackCard key={track.id} track={track}></TrackCard>
+        })}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
